6167c04370eed50438a6362cb219648dd88c3f33
Joseph <fsujoseph@gmail.com>
2021-11-30 17:37:11 -0500

    Refactored code

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ def index():
 def customers():
     db_connection = connect_to_database()
     if request.method == 'GET':
-        customer_query = "SELECT * from Customers;"
-        customer_result = execute_query(db_connection, customer_query).fetchall()
-        list_query = "SELECT customerID from Customers;"
-        list_result = execute_query(db_connection, list_query).fetchall()
-        return render_template('customers.html', customer_data=customer_result, list_data=list_result)
+        queryCustomers = "SELECT * from Customers;"
+        customerData = execute_query(db_connection, queryCustomers).fetchall()
+        queryCustIds = "SELECT customerID from Customers;"
+        customerIds = execute_query(db_connection, queryCustIds).fetchall()
+        return render_template('customers.html', customer_data=customerData, list_data=customerIds)
 
     elif request.method == 'POST':
         email = request.form.get("email")
@@ -33,12 +33,17 @@ def customers():
         state = request.form.get("state")
         phone = request.form.get("phone")
         citizenID = request.form.get("citizenID")
-        customerID = request.form.get("customerID")
+        searchCustomerID = request.form.get("searchCustomerID")
+        deleteCustomerID = request.form.get("deleteCustomerID")
         data = (email, fname, lname, address, zip, city, state, phone, citizenID)
-        if customerID:
+        if searchCustomerID:
             query = "SELECT * FROM Customers WHERE customerID = %s;"
-            customer = execute_query(db_connection, query, customerID)
+            customer = execute_query(db_connection, query, searchCustomerID)
             return render_template('customers.html', select_customer=customer)
+
+        elif deleteCustomerID:
+            query = "DELETE FROM Customers WHERE customerID = %s;"
+            execute_query(db_connection, query, deleteCustomerID)
 
         elif None not in data:
             query = """INSERT INTO `Customers` (`customerEmail`, `customerFirstName`, `customerLastName`, `customerAddress`,
@@ -46,17 +51,6 @@ def customers():
                                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"""
             execute_query(db_connection, query, data)
         return redirect('customers.html')
-
-
-# following was used to fix an error that occured randomly when deleting customer rows.
-# https://stackoverflow.com/questions/21740359/python-mysqldb-typeerror-not-all-arguments-converted-during-string-formatting
-@app.route('/delete')
-def delete():
-    db_connection = connect_to_database()
-    query = "DELETE FROM Customers WHERE customerID = %s"
-    data = request.args.get('deletecustomerID')
-    execute_query(db_connection, query, [data])
-    return redirect('customers.html')
 
 
 @app.route('/orders.html', methods=["GET", "POST"])
@@ -103,32 +97,22 @@ def products():
         productPrice = request.form.get("price")
         productStock = request.form.get("stock")
 
-        query = 'INSERT INTO Products (productName, productPrice, productStock) VALUES (%s,%s,%s)'
-        data = (productName, productPrice, productStock)
-        if None not in data:
-            execute_query(db_connection, query, data)
+        productID = request.form.get("productID")
+        updateName = request.form.get("updateName")
+        updatePrice = request.form.get("updatePrice")
+        updateStock = request.form.get("updateStock")
+
+        addProduct = (productName, productPrice, productStock)
+        updateProduct = (updateName, updatePrice, updateStock, productID)
+        if None not in addProduct:
+            query = 'INSERT INTO Products (productName, productPrice, productStock) VALUES (%s,%s,%s)'
+            execute_query(db_connection, query, addProduct)
+
+        elif None not in updateProduct:
+            query = "UPDATE Products SET productName=%s, productPrice=%s, productStock=%s WHERE productID=%s"
+            execute_query(db_connection, query, updateProduct)
+
         return redirect('products.html')
-
-
-@app.route('/update', methods=["GET", "POST"])
-def update():
-    db_connection = connect_to_database()
-    if request.method == 'GET':
-        data = request.args.get('updateproductID')
-        query = 'SELECT productID, productName, productPrice, productStock FROM Products WHERE productID = %s' % data
-        result = execute_query(db_connection, query).fetchone()
-        return render_template('productupdate.html', data=result)
-
-    elif request.method == 'POST':
-        name = request.form.get("updateproductName")
-        price = request.form.get("updateproductPrice")
-        stock = request.form.get("updateproductStock")
-        prodID = request.form.get("updateproductID")
-        query = "UPDATE Products SET productName=%s, productPrice=%s, productStock=%s WHERE productID=%s"
-        data = (name, price, stock, prodID)
-        if None not in data:
-            execute_query(db_connection, query, data)
-        return redirect('/products.html')
 
 
 @app.route('/suppliers.html', methods=["GET", "POST"])
